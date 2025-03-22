c346356009971be32eb2389b1339e885255b5b7f
Shahina sheikh <168347585+Inshiya1904@users.noreply.github.com>
2025-03-22 22:44:26 +0530

    Update SocketContext.jsx

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://chat-app-1-44ne.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
